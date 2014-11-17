2d43a10a104be7965b1c78f4fb0453537d2be36b
davidbarbeiro <david.gdbarbeiro@gmail.com>
2014-11-17 02:26:13 +0000

    topic subscriber updated

--- ServerPlayers-EJB/ejbModule/mdb/TopicSubscriber.java
+++ ServerPlayers-EJB/ejbModule/mdb/TopicSubscriber.java
@@ -11,6 +11,7 @@ import javax.jms.ObjectMessage;
 import javax.jms.MessageListener;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  * Message-Driven Bean implementation class for: TopicSubscriber
@@ -25,12 +26,10 @@ public class TopicSubscriber implements MessageListener {
 	@PersistenceContext(name="TestPersistence")
 	EntityManager em;
 	
-	int test;
     /**
      * Default constructor. 
      */
     public TopicSubscriber() {
-        // TODO Auto-generated constructor stub
     }
 	
 	/**
@@ -44,32 +43,56 @@ public class TopicSubscriber implements MessageListener {
 			globalNewsList = (original.models.NewsList)obj.getObject();
 		} catch (JMSException e) {
 			e.printStackTrace();
-		}
-
-        
+		}        
+        //The received message contains a list of sections which contains a list of news
         ArrayList<original.models.NewsSection> sections = globalNewsList.getSections();
-        ArrayList<models.News> DBnewsList = new ArrayList<models.News>(); 
         for (original.models.NewsSection newsSection : sections) {
-			List<original.models.News> newsList = newsSection.getNews();
+			List<original.models.News> newsList = newsSection.getNews();			
 			for (original.models.News news : newsList) {
-				models.News tempNews = new models.News();
-				DBnewsList.add(tempNews);
-				tempNews.setTitle(news.getTitle());
-				//tempNews.setAuthor(news.getAuthor());
-				tempNews.setHighlights(news.getHighlights());
-				tempNews.setLink(news.getLink());
-				tempNews.setText(news.getText());
-				tempNews.setDate(news.getDate());
-				tempNews.setMediaURL(news.getMedia_Url());
-				tempNews.setSection(newsSection.getName());
-				em.persist(tempNews);
+				//We only add news that are unique in their title&date
+				Query query = em.createQuery("Select n FROM News n WHERE n.title = :title and n.date = :date");
+				query.setParameter("title", news.getTitle());
+				query.setParameter("date", news.getDate());
+				models.News tempNews=null;
+				try{
+					tempNews = (models.News) query.getSingleResult();
+				}catch(Exception e){
+					//System.out.println( e.getMessage() );
+				}
+				if(tempNews==null){
+					tempNews = new models.News();
+					original.models.Authors authors1 = news.getAuthors();
+					List<String> authors = authors1.getAuthors();
+					ArrayList<models.Author> authorsDB=new ArrayList<models.Author>();
+					for (String name : authors) {
+						query = em.createQuery("Select a FROM Author a WHERE a.name = :name");
+						query.setParameter("name", name);
+						models.Author author=null;
+						try{
+							author = (models.Author)query.getSingleResult();
+						}catch(Exception e){
+							System.out.println( e.getMessage() );
+						}
+						if(author==null){
+							author = new models.Author(name);
+							em.persist(author);
+						}
+						authorsDB.add(author);
+					}
+					tempNews.setAuthors(authorsDB);
+					tempNews.setTitle(news.getTitle());
+					tempNews.setHighlights(news.getHighlights().getHighlights());
+					tempNews.setLink(news.getLink());
+					tempNews.setText(news.getText());
+					tempNews.setDate(news.getDate());
+					tempNews.setMediaURL(news.getMedia_Url());
+					tempNews.setSection(newsSection.getName());
+					em.persist(tempNews);
+				}
 			}
 		}
         
         System.out.println("Database populated");
-        
-        
-
     }
 
 }
